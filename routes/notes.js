642e81035d029f205bf02d8e8e2a234793518f73
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-10 14:55:29 -0700

    fix bug - saved notes now appear on left column

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,14 @@
 const notes = require('express').Router();
 const fs = require ('fs');
 const shortUniqueId = require('short-unique-id');
+const path = require('path');
 
 //'GET /api/notes/ should read the 'db.json' file & return all saved notes as JSON
 notes.get('/', (req, res) => {
     console.info(`${req.method} request received for notes`);
-    fs.readFile('./db/db.json', 'utf8', (data) => res.json(JSON.parse(data)));
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        console.log(data);
+        res.json(JSON.parse(data))});
 })
 
 //'POST /api/notes' receive a new note to save on request body + add to 'db.json' file + return the new note to the client + give each note a unique id when it's saved (npm packages?)
@@ -23,7 +26,7 @@ notes.post('/', (req, res) => {
             note_id: uid(),
         }
 
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
             if (err) {
                 console.log(err);
             } else {
@@ -31,7 +34,7 @@ notes.post('/', (req, res) => {
 
                 parsedNotes.push(newNote);
 
-                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (err) => err ? console.log (err) : console.log(`New note added!`))
+                fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(parsedNotes, null, 4), (err) => err ? console.log (err) : console.log(`New note added!`))
             }
         })
     } else {
